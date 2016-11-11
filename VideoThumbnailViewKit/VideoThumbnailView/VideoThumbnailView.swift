b965373bdd62dc33c3b843187ab79629c13d684d
Chuiko <user@MacBook-Pro-user.local>
2016-11-11 15:36:44 +0300

    Swift 3 and onTap delegate

--- VideoThumbnailViewKit/VideoThumbnailView/VideoThumbnailView.swift
+++ VideoThumbnailViewKit/VideoThumbnailView/VideoThumbnailView.swift
@@ -9,112 +9,132 @@
 import UIKit
 import AVFoundation
 
-public class VideoThumbnailView: UIView {
+public protocol VideoThumbnailViewDelegate {
+    func videoThumbnailViewGetImg(imgView:UIImage?) -> Void
+}
 
-  private var videoScroll = UIScrollView()
-  private var thumImageView = UIImageView()
-  private var arrThumbViews = NSMutableArray()
-  private var scrollContentWidth = 0.0
-  private var videoURL = NSURL()
-  private var videoDuration = 0.0
-  private var activityIndicator = UIActivityIndicatorView()
+open class VideoThumbnailView: UIView {
+    var delegate:VideoThumbnailViewDelegate? = nil
 
-  init(frame: CGRect, videoURL url: NSURL, thumbImageWidth thumbWidth: Double) {
-    super.init(frame: frame)
-    self.videoURL = url
-    self.videoDuration = self.getVideoTime(url)
-    activityIndicator = UIActivityIndicatorView(
-      frame: CGRect(
-        x: self.center.x - 15,
-        y: self.frame.size.height / 2 - 15,
-        width: 30.0,
-        height: 30.0))
-    activityIndicator.hidesWhenStopped = true
-    activityIndicator.activityIndicatorViewStyle = .White
-    activityIndicator.startAnimating()
-    addSubview(self.activityIndicator)
+    fileprivate var videoScroll = UIScrollView()
+    fileprivate var thumImageView = UIImageView()
+    fileprivate var arrThumbViews:[UIImageView] = []
+    fileprivate var scrollContentWidth = 0.0
+    fileprivate var videoURL = URL(string: "")
+    fileprivate var videoDuration = 0.0
+    fileprivate var activityIndicator = UIActivityIndicatorView()
 
-    videoScroll = UIScrollView(
-      frame: CGRect(
-        x: 0.0,
-        y: 0.0,
-        width: self.frame.size.width,
-        height: self.frame.size.height))
-    videoScroll.showsHorizontalScrollIndicator = false
-    videoScroll.showsVerticalScrollIndicator = false
-    videoScroll.bouncesZoom = false
-    videoScroll.bounces = false
-    self.addSubview(videoScroll)
+    public init(frame: CGRect, videoURL url: URL, thumbImageWidth thumbWidth: Double) {
+        super.init(frame: frame)
+        self.videoURL = url
+        self.videoDuration = self.getVideoTime(url)
+        activityIndicator = UIActivityIndicatorView(
+            frame: CGRect(
+                x: self.center.x - 15,
+                y: self.frame.size.height / 2 - 15,
+                width: 30.0,
+                height: 30.0))
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = .gray
+        activityIndicator.startAnimating()
+        addSubview(self.activityIndicator)
 
-    self.thumbImageProcessing(
-      videoUrl: videoURL,
-      thumbWidth: thumbWidth) { (thumbImages, error) -> Void in
-      // println(thumbImages)
-    }
+        videoScroll = UIScrollView(
+            frame: CGRect(
+                x: 0.0,
+                y: 0.0,
+                width: self.frame.size.width,
+                height: self.frame.size.height))
+        videoScroll.showsHorizontalScrollIndicator = false
+        videoScroll.showsVerticalScrollIndicator = false
+        videoScroll.bouncesZoom = false
+        videoScroll.bounces = false
+        self.addSubview(videoScroll)
 
-    self.layer.masksToBounds = true
-  }
-
-  private func thumbImageProcessing(
-    videoUrl url: NSURL,
-    thumbWidth: Double,
-    completion: ((thumbImages: NSMutableArray?, error: NSError?) -> Void)?) {
-    let priority = DISPATCH_QUEUE_PRIORITY_HIGH
-    dispatch_async(dispatch_get_global_queue(priority, 0)) {
-      for index in 0...Int(self.videoDuration) {
-        let thumbXCoords = Double(index) * thumbWidth
-        self.thumImageView = UIImageView(
-          frame: CGRect(
-            x: thumbXCoords,
-            y: 0.0,
-            width: thumbWidth,
-            height: Double(self.frame.size.height)))
-        self.thumImageView.contentMode = .ScaleAspectFit
-        self.thumImageView.backgroundColor = UIColor.clearColor()
-        self.thumImageView.layer.borderColor = UIColor.grayColor().CGColor
-        self.thumImageView.layer.borderWidth = 0.25
-        self.thumImageView.tag = index
-        self.thumImageView.image = self.generateVideoThumbs(
-          self.videoURL,
-          second: Double(index),
-          thumbWidth: thumbWidth)
-        self.scrollContentWidth = self.scrollContentWidth + thumbWidth
-        self.videoScroll.addSubview(self.thumImageView)
-        self.videoScroll.sendSubviewToBack(self.thumImageView)
-        if let imageView = self.thumImageView as UIImageView? {
-          self.arrThumbViews.addObject(imageView)
+        self.thumbImageProcessing(
+            videoUrl: videoURL!,
+            thumbWidth: thumbWidth) { (thumbImages, error) -> Void in
+                print (thumbImages.count)
+                self.delegate?.videoThumbnailViewGetImg(imgView: thumbImages.first?.image)
         }
-      }
-      self.videoScroll.contentSize = CGSize(
-        width: Double(self.scrollContentWidth),
-        height: Double(self.frame.size.height))
-      self.activityIndicator.stopAnimating()
-      completion?(thumbImages: self.arrThumbViews, error: nil)
-      self.videoScroll.setContentOffset(CGPoint(x: 0.0, y: 0.0), animated: true)
+
+        self.layer.masksToBounds = true
     }
-  }
 
-  private func getVideoTime(url: NSURL) -> Float64 {
-    let videoTime = AVURLAsset(URL: url, options: nil)
-    return CMTimeGetSeconds(videoTime.duration)
-  }
-
-  private func generateVideoThumbs(url: NSURL, second: Float64, thumbWidth: Double) -> UIImage {
-    let asset = AVURLAsset(URL: url, options: nil)
-    let generator = AVAssetImageGenerator(asset: asset)
-    generator.maximumSize = CGSize(width: thumbWidth, height: Double(self.frame.size.height))
-    generator.appliesPreferredTrackTransform = false
-    let thumbTime = CMTimeMakeWithSeconds(second, 1)
-    do {
-    let ref = try generator.copyCGImageAtTime(thumbTime, actualTime: nil)
-      return UIImage(CGImage: ref)
-    }catch {
-      print(error)
+    func handleTap(_ sender : UITapGestureRecognizer) {
+        let imgView = sender.view as? UIImageView
+        delegate?.videoThumbnailViewGetImg(imgView: imgView?.image)
     }
-    return UIImage()
-  }
 
-  required public init(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
+    fileprivate func thumbImageProcessing(
+        videoUrl url: URL,
+        thumbWidth: Double,
+        completion: ((_ thumbImages: [UIImageView], _ error: NSError?) -> Void)?) {
+        DispatchQueue.global(qos: .utility).async {
+            for index in 0...Int(self.videoDuration) {
+                let thumbXCoords = Double(index) * thumbWidth
+                self.thumImageView = UIImageView(
+                    frame: CGRect(
+                        x: thumbXCoords,
+                        y: 0.0,
+                        width: thumbWidth,
+                        height: Double(self.frame.size.height)))
+                self.thumImageView.contentMode = .scaleAspectFit
+                self.thumImageView.backgroundColor = UIColor.clear
+                self.thumImageView.layer.borderColor = UIColor.gray.cgColor
+                self.thumImageView.layer.borderWidth = 0.25
+                self.thumImageView.tag = index
+
+                let tapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(VideoThumbnailView.handleTap(_:)))
+                tapGestureRecognizer.numberOfTapsRequired = 1;
+
+                self.thumImageView.addGestureRecognizer(tapGestureRecognizer)
+                self.thumImageView.isUserInteractionEnabled = true
+
+                self.thumImageView.image = self.generateVideoThumbs(
+                    self.videoURL!,
+                    second: Double(index),
+                    thumbWidth: thumbWidth)
+                self.scrollContentWidth = self.scrollContentWidth + thumbWidth
+                self.videoScroll.addSubview(self.thumImageView)
+                self.videoScroll.sendSubview(toBack: self.thumImageView)
+                if let imageView = self.thumImageView as UIImageView? {
+                    self.arrThumbViews.append(imageView)
+                }
+                self.videoScroll.contentSize = CGSize(
+                    width: Double(self.scrollContentWidth),
+                    height: Double(self.frame.size.height))
+            }
+            self.videoScroll.contentSize = CGSize(
+                width: Double(self.scrollContentWidth),
+                height: Double(self.frame.size.height))
+            self.activityIndicator.stopAnimating()
+            completion?(self.arrThumbViews, nil)
+            self.videoScroll.setContentOffset(CGPoint(x: 0.0, y: 0.0), animated: true)
+        }
+    }
+
+    fileprivate func getVideoTime(_ url: URL) -> Float64 {
+        let videoTime = AVURLAsset(url: url, options: nil)
+        return CMTimeGetSeconds(videoTime.duration)
+    }
+
+    fileprivate func generateVideoThumbs(_ url: URL, second: Float64, thumbWidth: Double) -> UIImage {
+        let asset = AVURLAsset(url: url, options: nil)
+        let generator = AVAssetImageGenerator(asset: asset)
+        generator.maximumSize = CGSize(width: thumbWidth, height: Double(self.frame.size.height))
+        generator.appliesPreferredTrackTransform = false
+        let thumbTime = CMTimeMakeWithSeconds(second, 1)
+        do {
+            let ref = try generator.copyCGImage(at: thumbTime, actualTime: nil)
+            return UIImage(cgImage: ref)
+        }catch {
+            print(error)
+        }
+        return UIImage()
+    }
+
+    required public init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
